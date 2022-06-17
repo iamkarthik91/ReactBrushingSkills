99d5181ac743afe43736dad81f449cd97ee7b3b0
iamkarthik91 <88312637+iamkarthik91@users.noreply.github.com>
2022-06-17 12:44:41 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Karthik S</h1>
+    <ul>
+      <li>React Developer</li>
+      <li>Brushing skills</li>
+      <li>Having Fun coding</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
